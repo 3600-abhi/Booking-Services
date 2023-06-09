3448be23b8b13abdbafce1903c6e49bd292d7c2c
3600-abhi <abhishekjaiswal3600@gmail.com>
2023-06-09 12:56:41 +0530

    started booking implementation

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,1 +1,27 @@
-module.exports = {};
+const { StatusCodes } = require("http-status-codes");
+const { ErrorResponse, SuccessResponse } = require("../utils/common");
+const { BookingService } = require("../services");
+
+async function createBooking(req, res) {
+  try {
+    const response = await BookingService.createBooking({
+      flightId: req.body.flightId,
+      userId: req.body.userId,
+      noOfSeats: req.body.noOfSeats,
+      totalCost: req.body.totalCost,
+    });
+
+    console.log("inside-controller and response : ", response);
+
+    SuccessResponse.data = response;
+    SuccessResponse.message = "Successfully completed the booking";
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error; // this error object is (AppError) object
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+module.exports = {
+  createBooking,
+};
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,1 +1,47 @@
-module.exports = {};
+const axios = require("axios");
+const { StatusCodes } = require("http-status-codes");
+const { BookingRepository } = require("../repositories");
+const { ServerConfig } = require("../config");
+const db = require("../models");
+const AppError = require("../utils/errors/app-errors");
+
+async function createBooking(data) {
+  try {
+    const result = await db.sequelize.transaction(
+      async function bookingImplementation(t) {
+        const flight = await axios.get(
+          `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`
+        );
+
+        const flightData = flight.data.data;
+
+        console.log("remainingSeats : ", flightData.remainingSeats);
+
+        if (data.noOfSeats > flightData.remainingSeats) {
+          throw new AppError(
+            "Not enough seats available",
+            StatusCodes.BAD_REQUEST
+          );
+        }
+
+        return "Booked";
+      }
+    );
+
+    return result;
+  } catch (error) {
+    // this error is thrown form try-block if noOfSeats exceeds remainingSeats in flight
+    if (error.statusCode === StatusCodes.BAD_REQUEST) {
+      throw error;
+    }
+
+    throw new AppError(
+      "Cannot book the flight",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+module.exports = {
+  createBooking,
+};

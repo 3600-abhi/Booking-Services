a4c054d1bad0b115f53c81d17e8b25516fa0975b
3600-abhi <abhishekjaiswal3600@gmail.com>
2023-06-10 11:25:01 +0530

    FEAT: Added cancel booking logic

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -65,6 +65,43 @@ async function createBooking(data) {
   }
 }
 
+async function cancelBooking(bookingId) {
+  const transaction = await db.sequelize.transaction();
+
+  try {
+    const bookingDetails = await bookingRepository.get(bookingId, transaction);
+
+    if (bookingDetails.status === CANCELLED) {
+      await transaction.commit();
+      return true;
+    }
+
+    console.log("starting to increase the seats");
+
+    // Increment the number of seats in the corresponding flight
+    await axios.patch(
+      `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${bookingDetails.flightId}/seats`,
+      {
+        seats: bookingDetails.noOfSeats,
+        toDecrease: false,
+      }
+    );
+
+    console.log("after increaseing the seats");
+
+    await bookingRepository.update(
+      bookingId,
+      { status: CANCELLED },
+      transaction
+    );
+
+    await transaction.commit();
+  } catch (error) {
+    await transaction.rollback();
+    throw error;
+  }
+}
+
 async function makePayment(data) {
   const transaction = await db.sequelize.transaction();
 
@@ -73,6 +110,20 @@ async function makePayment(data) {
       data.bookingId,
       transaction
     );
+
+    if (data.totalCost !== bookingDetails.totalCost) {
+      throw new AppError(
+        "The amount of the payment does not match",
+        StatusCodes.BAD_REQUEST
+      );
+    }
+
+    if (data.userId !== bookingDetails.userId) {
+      throw new AppError(
+        "The user corresponding to the booking does not match",
+        StatusCodes.BAD_REQUEST
+      );
+    }
 
     if (bookingDetails.status === BOOKED) {
       throw new AppError(
@@ -90,27 +141,8 @@ async function makePayment(data) {
     const fiveMinutesInMilliseconds = 300000;
 
     if (currentTime - bookingTime > fiveMinutesInMilliseconds) {
-      await bookingRepository.update(
-        data.bookingId,
-        { status: CANCELLED },
-        transaction
-      );
-
+      await cancelBooking(data.bookingId);
       throw new AppError("The booking has expired", StatusCodes.BAD_REQUEST);
-    }
-
-    if (data.totalCost !== bookingDetails.totalCost) {
-      throw new AppError(
-        "The amount of the payment does not match",
-        StatusCodes.BAD_REQUEST
-      );
-    }
-
-    if (data.userId !== bookingDetails.userId) {
-      throw new AppError(
-        "The user corresponding to the booking does not match",
-        StatusCodes.BAD_REQUEST
-      );
     }
 
     // we assume here that payment is successfull
@@ -138,5 +170,6 @@ async function makePayment(data) {
 
 module.exports = {
   createBooking,
+  cancelBooking,
   makePayment,
 };
